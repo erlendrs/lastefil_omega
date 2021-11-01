38e50f3d3a5984a2101357d52500e071b6a856e9
Erlend Rånes Salte <erlendranessalte@Erlends-MacBook-Air.local>
2021-11-01 20:06:13 +0100

    første utkast av doct dict

---
+++ config.py
@@ -0,0 +1,8 @@
+
+
+doc_dict = {'RP': ['Prøveprotokoll', 'ANLEGGSDOK', 'PRPROT'],
+            'XD': ['Målskisse', 'TEGNINGER', 'MONT'],
+            'XF': ['Fundamenttegning', 'TEGNINGER', 'FUNDT'],
+            'XK': ['Interne strømløpsskjema', 'TEGNINGER', 'SKJEMA'],
+            'XQ': ['Stativtegning', 'TEGNINGER', 'MONT'],
+                }
